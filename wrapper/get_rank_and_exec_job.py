049a9da24b8af777f4fa89be6ec96e8933a56be2
Pavlo Svirin <pavlo.svirin@cern.ch>
2016-07-30 17:03:52 -0400

    Added workdir variable to MPI startup, full path of script for call

--- wrapper/get_rank_and_exec_job.py
+++ wrapper/get_rank_and_exec_job.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3.4
 import os
+import sys
 from mpi4py import MPI
 from subprocess import call
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+mydir = os.path.dirname(os.path.realpath(__file__))
+workdir = sys.argv[0]
 
 print(rank)
 #call(["./alien_job.sh", str(rank)])
-call(["./alien_job_run.sh", str(rank)])
+call([mydir + "/alien_job_run.sh", str(rank), workdir])
