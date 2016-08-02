c05a43c5b1e7ed72f4db65ee05ec77636f41ede9
Pavlo Svirin <pavlo.svirin@cern.ch>
2016-08-02 13:55:11 -0400

    First approach test for multibatch passed

--- wrapper/get_rank_and_exec_job.py
+++ wrapper/get_rank_and_exec_job.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3.5
 import os
 import sys
 from mpi4py import MPI
@@ -7,8 +7,11 @@ from subprocess import call
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 mydir = os.path.dirname(os.path.realpath(__file__))
-workdir = sys.argv[0]
+workdir = sys.argv[2]
+scripts_dir = sys.argv[1]
 
-print(rank)
+print("Rank: " + str(rank))
+print("workdir: " + workdir)
+print("scripts dir" + scripts_dir)
 #call(["./alien_job.sh", str(rank)])
-call([mydir + "/alien_job_run.sh", str(rank), workdir])
+call([scripts_dir + "/alien_multijob_run.sh", str(rank), workdir])
